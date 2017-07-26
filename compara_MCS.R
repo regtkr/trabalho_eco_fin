edc218d18f746121b4e0c2bda6c16602fbd1413d
regtkr <regiskurokawa+github@gmail.com>
2017-07-26 03:24:26 -0300

    Volatilidade Estocástica completamente funcional!

--- compara_MCS.R
+++ compara_MCS.R
@@ -17,6 +17,12 @@ library(stochvol)
 #----------------------------------------------------------------------
 dados = read.table("/home/regis/Dropbox/econometriafinanças2017/parte3-volatilidade/sp500.txt",header=F)
 ret = diff(log(dados$V1))
+
+# selecionando as primeiras linhas para ser mais rápido
+faixa = 1:2000
+
+ret = ret[faixa]
+
 tret = ts(ret, start = c(1980,1,1), frequency = 365) # Data ficticia, melhorar
 
 
@@ -63,7 +69,7 @@ spec[11] = ugarchspec(variance.model = list(model = "fGARCH", submodel = "GJRGAR
 #----------------------------------------------------------------------
 # VaR
 #----------------------------------------------------------------------
-loss = data.frame(matrix(, nrow=2557, ncol=0))
+loss = data.frame(matrix(, nrow=500, ncol=0))
 
 for (i in 1:length(spec)){
 	# Nome dos modelo
@@ -71,7 +77,7 @@ for (i in 1:length(spec)){
 	print(model_name)
 
 	# Aplicando a janela móvel
-	roll = ugarchroll(spec[[i]], data = tret, n.ahead = 1, n.start = 5000, VaR.alpha = c(0.05), refit.every = 500)
+	roll = ugarchroll(spec[[i]], data = tret, n.ahead = 1, n.start = 1500, VaR.alpha = c(0.05), refit.every = 50)
 
 	# Extraindo o VaR
 	VaR = as.data.frame(roll, which="VaR")
@@ -108,8 +114,8 @@ for (i in 12:15) {
 	# Aplicando a janela móvel
 	roll = UniGASRoll(data = tret, 
 		GASSpec = spec[[i]], 
-		Nstart = 5000, 
-		RefitEvery = 500, 
+		Nstart = 1500, 
+		RefitEvery = 50, 
 		RefitWindow = c("moving"))
 
 	# Extraindo o VaR
@@ -131,44 +137,39 @@ for (i in 12:15) {
 #----------------------------------------------------------------------
 # MODELOS
 #----------------------------------------------------------------------
-faixa = 1:2000
-tret = tret[faixa]
-periodo = 1:1500
 
 m = mean(tret)
-
-treino = tret[ periodo] - m
-teste  = tret[-periodo] - m
-
-VaR = vector(mode = "numeric", length = 500)
-
-for (i in 1:length(VaR)) {
-	plot(i)
-
-	draws = svsample(
-	tret[seq(i,1500+i-1)],
-	draws = 1000, 
-	burnin = 100, 
-	priormu = c(-10, 1), 
-	priorphi = c(20, 1.2), 
-	priorsigma = 0.2,
-	designmatrix = "ar1")
-
-prev = predict(draws, step = 1)
-
-previsao = arpredict(draws, prev)
-
-VaR_sim[i] = quantile(previsao, 0.05) + m
-
-}
-
-plot(teste+m, type = 'l', col = 'red')
-lines(VaR_sim)
 
 #----------------------------------------------------------------------
 # VaR
 #----------------------------------------------------------------------
 
+VaR_sim = vector(mode = "numeric", length = 500)
+
+for (i in 1:length(VaR_sim)) {
+	print(i)
+
+	draws = svsample(
+		tret[seq(i,1500+i-1)] - m,
+		draws = 1000, 
+		burnin = 100, 
+		priormu = c(-10, 1), 
+		priorphi = c(20, 1.2), 
+		priorsigma = 0.2,
+		designmatrix = "ar1", 
+		quiet = TRUE)
+
+	prev = predict(draws, step = 1)
+
+	previsao = arpredict(draws, prev)
+
+	VaR_sim[i] = quantile(previsao, 0.05) + m
+}
+
+plot(tret, type = 'l', col = 'red')
+lines(VaR_sim)
+
+loss['SV'] = LossVaR(VaR_real, VaR_sim, tau = 0.05)
 
 
 #----------------------------------------------------------------------
